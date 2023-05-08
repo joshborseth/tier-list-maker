11bcaa2f05dc7eb45bedddb84010555bf9eebeb3
joshborseth <joshuaborseth@gmail.com>
2023-05-07 18:26:49 -0700

    delete endpoint added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 const Home: NextPage = () => {
   return (
-    <>
+    <div className="flex h-screen w-screen flex-col justify-between">
       <Head>
         <title>Tier List of Snuggles | Home A Snuggler</title>
         <meta
@@ -14,22 +14,18 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="flex min-h-screen flex-col items-center justify-start">
-        <header className="flex items-center justify-center gap-10 py-5">
-          <h1 className="text-4xl font-bold">Snuggler Tier List</h1>
-          <Link
-            href="/create"
-            className="rounded-full bg-black px-4 py-2 text-white"
-          >
-            Add a Snuggler
-          </Link>
-        </header>
-        <main className="w-full">
-          <TierList />
-          <SnugglerSelectionField />
-        </main>
-      </div>
-    </>
+      <header className="flex items-center justify-center gap-10 px-8 py-5">
+        <h1 className="text-4xl font-bold">Snuggler Tier List</h1>
+        <Link
+          href="/create"
+          className="rounded-full bg-black px-4 py-2 text-center text-white"
+        >
+          Add a Snuggler
+        </Link>
+      </header>
+      <TierList />
+      <SnugglerSelectionField />
+    </div>
   );
 };
 
@@ -37,7 +33,7 @@ export default Home;
 
 const TierList = () => {
   return (
-    <div className="w-full">
+    <div className="flex flex-grow flex-col">
       <TierRow tier="S" className="bg-red-400" />
       <TierRow tier="A" className="bg-orange-400" />
       <TierRow tier="B" className="bg-amber-400" />
@@ -50,8 +46,8 @@ const TierList = () => {
 
 const TierRow = ({ tier, className }: { tier: string; className: string }) => {
   return (
-    <div className={`py-8 ${className}`}>
-      <p className="inline border-r-2 border-black px-8 py-2 text-xl font-bold">
+    <div className={`${className} flex flex-grow items-center justify-start`}>
+      <p className="inline border-r-2 border-black px-8 text-xl font-bold">
         {tier}
       </p>
     </div>
@@ -61,14 +57,26 @@ const TierRow = ({ tier, className }: { tier: string; className: string }) => {
 const SnugglerSelectionField = () => {
   const { data: snugglers } = api.entry.get.useQuery();
   return (
-    <div className="w-full">
-      {snugglers?.map((snuggler) => (
-        <div key={snuggler.id}>
-          <div>{snuggler.name}</div>
-          <p>{snuggler.description}</p>
-          <Image src={snuggler?.imageUrl} alt={snuggler.name} />
-        </div>
-      ))}
+    <div className="flex gap-2 p-4">
+      {snugglers &&
+        snugglers?.map((snuggler) => (
+          <div key={snuggler.id}>
+            <div className="relative items-center justify-center">
+              <div className="absolute bottom-0 left-0 right-0 top-0 z-20 flex h-40 w-40 flex-col items-center justify-center rounded-full bg-black bg-opacity-0 p-3 text-center text-white opacity-0 transition-all hover:bg-opacity-75 hover:opacity-100">
+                <div className="text-2xl font-bold">{snuggler.name}</div>
+                <p>{snuggler.description}</p>
+              </div>
+              <Image
+                width={300}
+                height={300}
+                src={snuggler?.imageUrl}
+                alt={snuggler.name}
+                className="h-40 w-40 rounded-full object-cover"
+                draggable="false"
+              />
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
--- src/server/api/routers/entry.ts
+++ src/server/api/routers/entry.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 import { db } from "../../../db/connection";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { entry } from "../../../db/schema";
+import { eq } from "drizzle-orm";
 export const entryRouter = createTRPCRouter({
   create: publicProcedure
     .input(
@@ -17,4 +18,14 @@ export const entryRouter = createTRPCRouter({
   get: publicProcedure.query(async () => {
     return await db.select().from(entry);
   }),
+  //TODO: make this a protected procedure
+  delete: publicProcedure
+    .input(
+      z.object({
+        id: z.number(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      return await db.delete(entry).where(eq(entry.id, input.id));
+    }),
 });
